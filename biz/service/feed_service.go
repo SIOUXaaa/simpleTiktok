e05056bcd78c9bf3d071fac0fd1007c715db0136
sunzhengyu99 <sunzy9901@qq.com>
2023-08-23 12:40:07 +0800

    init feed_service.go

--- biz/service/feed_service.go
+++ biz/service/feed_service.go
@@ -1,1 +1,24 @@
 package service
+
+import (
+	"context"
+	"github.com/cloudwego/hertz/pkg/app"
+	"simpleTiktok/biz/model/basic/feed"
+)
+
+type FeedService struct {
+	ctx context.Context     // 传递上下文信息
+	c   *app.RequestContext // 请求信息
+}
+
+func NewFeedService(ctx context.Context, c *app.RequestContext) *FeedService {
+	return &FeedService{
+		ctx: ctx,
+		c:   c,
+	}
+}
+
+func (s *FeedService) Feed(req *feed.DouyinFeedRequest) (*feed.DouyinFeedRequest, error) {
+
+	return req, nil
+}
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.20
 
 require (
 	github.com/cloudwego/hertz v0.6.7
+	github.com/sirupsen/logrus v1.9.3
 	golang.org/x/crypto v0.7.0
 	google.golang.org/protobuf v1.31.0
 	gorm.io/driver/mysql v1.5.1
