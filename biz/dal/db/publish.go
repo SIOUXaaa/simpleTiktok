5173df0c8ae43313908d919c97ff8000d3fd11df
sunzhengyu99 <sunzy9901@qq.com>
2023-08-23 14:45:41 +0800

    add db.GetVideosByLastTime

--- biz/dal/db/publish.go
+++ biz/dal/db/publish.go
@@ -1,10 +1,9 @@
 package db
 
 import (
-	"time"
 	"simpleTiktok/pkg/constants"
+	"time"
 )
-
 
 type Video struct {
 	ID          int64
@@ -25,4 +24,13 @@ func CreateVideo(video *Video) (Video_id int64, err error) {
 		return 0, err
 	}
 	return video.ID, err
+}
+
+func GetVideosByLastTime(LastTime time.Time) ([]*Video, error) {
+	videos := make([]*Video, constants.VideoFeedCount)
+	err := DB.Where("publish_time < ?", LastTime).Order("publish_time desc").Limit(constants.VideoFeedCount).Find(&videos).Error
+	if err != nil {
+		return nil, err
+	}
+	return videos, nil
 }
