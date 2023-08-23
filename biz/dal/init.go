184347c93eb912f086076a3e2885d1bc0549694e
SIOUXaaa <3011057139@qq.com>
2023-08-23 17:31:13 +0800

    completed minio and some function of minio in biz/dal/minio

--- biz/dal/init.go
+++ biz/dal/init.go
@@ -2,9 +2,11 @@ package dal
 
 import (
 	"simpleTiktok/biz/dal/db"
+	"simpleTiktok/biz/dal/minio"
 )
 
 func Init() {
 	db.Init()
 	db.DB.AutoMigrate(&db.User{}, &db.Video{}, &db.Comment{}, &db.Favorites{})
+	minio.Init()
 }
