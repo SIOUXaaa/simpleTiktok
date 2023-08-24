a4f3bb0640c43071b45d50207b7f91d400789ef8
dzr <652614345@qq.com>
2023-08-24 17:23:40 +0800

    add favorite action

--- biz/service/favorite_service.go
+++ biz/service/favorite_service.go
@@ -1,1 +1,60 @@
 package service
+
+import (
+	"context"
+	"simpleTiktok/biz/dal/db"
+	"simpleTiktok/biz/model/interact/favorite"
+	"simpleTiktok/pkg/constants"
+	"simpleTiktok/pkg/errno"
+	"time"
+
+	"github.com/cloudwego/hertz/pkg/app"
+	"gorm.io/gorm"
+)
+
+type FavortieService struct {
+	ctx context.Context
+	c   *app.RequestContext
+}
+
+func NewFavoriteService(ctx context.Context, c *app.RequestContext) *FavortieService {
+	return &FavortieService{ctx, c}
+}
+
+func (f *FavortieService) Action(req *favorite.DouyinFavoriteActionRequest) error {
+	userId, _ := f.c.Get("current_user_id")
+	videoId := req.GetVideoId()
+	actionType := req.GetActionType()
+
+	favorite, err := db.QueryFavoriteByUserIdAndVedioId(userId.(int64), videoId)
+
+	if actionType == constants.FavoriteActionType {
+		if err == gorm.ErrRecordNotFound { //之前未给这条视频点赞，需要进行点赞操作
+			_, err := db.CreateFavoriteAndIncreaseVedioLikes(&db.Favorites{
+				UserId:    userId.(int64),
+				VideoId:   videoId,
+				CreatedAt: time.Now(),
+			})
+			if err != nil {
+				return err
+			}
+		} else if err != nil { //其他情况异常
+			return err
+		}
+		//如果已经给这条视频点赞了，就无需处理
+		return nil
+	} else if actionType == constants.UnFavoriteActionType {
+		if err == gorm.ErrRecordNotFound { //之前未给这条视频点赞，报错
+			return errno.FavoriteRelationNotExistErr
+		} else if err != nil { //其他情况异常
+			return err
+		}
+		//撤销点赞
+		_, err := db.DeleteFavoriteAndDecreaseVedioLikes(favorite)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
